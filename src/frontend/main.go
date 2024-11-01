d42b8632c716f087cf8a7a85524db13fd004359d
Serghei C. <81874080+ciudinsg@users.noreply.github.com>
2024-11-01 11:43:16 +0200

    Update main.go

--- src/frontend/main.go
+++ src/frontend/main.go
@@ -183,7 +183,7 @@ func main() {
 	r.HandleFunc(baseUrl + "/bot", svc.chatBotHandler).Methods(http.MethodPost)
   // Instrumenting the Frontend Microservice
 	// r.Handle("/metrics", promhttp.Handler()) // Exposes Prometheus metrics
-	r.HandleFunc(baseUrl + "/metrics", func(w http.ResponseWriter, _ *http.Request) { fmt.Fprint(w, "ok") })	
+	r.HandleFunc(baseUrl + "/metrics", func(w http.ResponseWriter, _ *http.Request) { fmt.Fprint(w, "User-agent: *\nDisallow: /") })
 
 	
 	var handler http.Handler = r
