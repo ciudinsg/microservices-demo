ce228f85f306b38151266ad29f31b62f84ea18d9
Serghei C. <81874080+ciudinsg@users.noreply.github.com>
2024-11-01 11:13:23 +0200

    Update main.go

--- src/frontend/main.go
+++ src/frontend/main.go
@@ -182,7 +182,8 @@ func main() {
 	r.HandleFunc(baseUrl + "/product-meta/{ids}", svc.getProductByID).Methods(http.MethodGet)
 	r.HandleFunc(baseUrl + "/bot", svc.chatBotHandler).Methods(http.MethodPost)
   // Instrumenting the Frontend Microservice
-	r.Handle("/metrics", promhttp.Handler()) // Exposes Prometheus metrics
+	// r.Handle("/metrics", promhttp.Handler()) // Exposes Prometheus metrics
+	r.HandleFunc(baseUrl + "/metrics", promhttp.Handler()) // Exposes Prometheus metrics
 	
 	var handler http.Handler = r
 	handler = &logHandler{log: log, next: handler}     // add logging
