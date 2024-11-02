6ecb2cdb8f96d2e5fb49b08b3d6aa13507db66b0
Serghei C. <81874080+ciudinsg@users.noreply.github.com>
2024-11-02 18:08:39 +0200

    Update main.go

--- src/frontend/main.go
+++ src/frontend/main.go
@@ -210,7 +210,7 @@ func main() {
 	// Instrumenting the Frontend Microservice
 	r.Handle("/metrics", promhttp.Handler()) // Exposes Prometheus metrics
 	// Prometheus endpoint
-	r.Path("/metrics").Handler(promhttp.Handler())
+	// r.Path("/metrics").Handler(promhttp.Handler())
 	// r.HandleFunc(baseUrl + "/ping", func(w http.ResponseWriter, _ *http.Request) { fmt.Fprint(w, "pong") })
 	//r.Path("/metrics").Handler(promhttp.Handler())
 
